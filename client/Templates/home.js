7e2d9e1e6f7ded02d770f950210689ef5252d85f
rathomas17 <rathomas17@gmail.com>
2016-02-16 19:14:00 -0500

    Changed contact us form. Changed video. Resized mobile version.

---
+++ client/Templates/home.js
@@ -0,0 +1,8 @@
+Template.home.events({
+//  document.getElementById("homevideo").addEventListener("click", myFunction)
+//  function myFunction() {
+//  alert ("Hello World!");
+//  }
+
+
+});
--- client/Templates/index.js
+++ client/Templates/index.js
@@ -1,6 +1,12 @@
-$(document).ready(function(){               
+$(document).ready(function(){
   $('.materialboxed').materialbox();
 });
 $(document).ready(function(){
   $('.materialboxed').materialbox();
+});
+
+Template.index.events({
+  name: "Ben Bitdiddle"
+
+
 });
