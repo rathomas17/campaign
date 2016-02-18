b48054a611f8d7311f4f5753fdb84b85c1761a7a
Kamari Wright <kamariw95@gmail.com>
2016-02-18 18:21:37 -0500

    Fixes slick issue

--- client/Templates/index.js
+++ client/Templates/index.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-  $('.materialboxed').materialbox();
+  //$('.materialboxed').materialbox();
   $('.wheel').slick({
 
   });
 });
 
+Template.index.onRendered(function() {
+  $('.wheel').slick({
+
+  });
+});
 
 Template.index.events({
   name: "Ben Bitdiddle"
